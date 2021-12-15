f0b51e1463e7dd16bc434254264ac4ce90e48e87
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-15 22:59:48 +0200

    An implementation using ternary

---
+++ Assignment03/Assignment03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assignment03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // && <- cand primul este false
+            // || <- cand primul este true
+            // conditie ? caz_true : caz_false
+            // variantaNotNull ?? alternativaPtNull
+
+            string text1, text2, text3;
+            text1 = Console.ReadLine();
+            string result = string.IsNullOrWhiteSpace(text1)
+                ? (
+                    string.IsNullOrWhiteSpace(text2 = Console.ReadLine())
+                    ? (
+                        string.IsNullOrWhiteSpace(text3 = Console.ReadLine())
+                            ? "Nu am continut"
+                            : text3
+                      )
+                    : text2
+                  )
+                : text1;
+
+            Console.WriteLine(result);
+        }
+    }
+}
